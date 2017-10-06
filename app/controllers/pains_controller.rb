d2fbedcaff375c48317f0bd0125c0af7bff7afff
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-10-06 18:28:28 +0800

    add permission check on show page

--- app/controllers/pains_controller.rb
+++ app/controllers/pains_controller.rb
@@ -1,5 +1,6 @@
 class PainsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_pain_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @pains = Pain.all
@@ -11,6 +12,8 @@ class PainsController < ApplicationController
 
   def create
     @pain = Pain.new(pain_params)
+    @pain.user = current_user
+
     if @pain.save
       redirect_to pains_path
     else
@@ -24,11 +27,10 @@ class PainsController < ApplicationController
     end
 
     def edit
-      @pain = Pain.find(params[:id])
     end
 
     def update
-      @pain = Pain.find(params[:id])
+
       if @pain.update(pain_params)
         redirect_to pains_path, notice: "修改成功啦！"
       else
@@ -37,18 +39,24 @@ class PainsController < ApplicationController
     end
 
    def destroy
-     @pain = Pain.find(params[:id])
+
      @pain.destroy
-     flash[:alert] = "已经删除了！"
-     redirect_to pains_path
+    #  flash[:alert] = "已经删除了！"
+     redirect_to pains_path, alert: "已经删除了！"
    end
 
   private
 
+  def find_pain_and_check_permission
+    @pain = Pain.find(params[:id])
+
+      if current_user != @pain.user
+        redirect_to root_path, alert: "你没有权限！"
+      end
+    end
+
   def pain_params
     params.require(:pain).permit(:total, :user_id, :pain_name, :pain_cause, :regret_level, :next_do)
   end
-
-
 
 end
--- app/models/pain.rb
+++ app/models/pain.rb
@@ -1,4 +1,5 @@
 class Pain < ApplicationRecord
   validates :pain_name, :pain_cause, :regret_level, :next_do,  presence: true
+  belongs_to :user
 
 end
